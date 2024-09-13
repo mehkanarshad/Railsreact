cafbd184c9a29d16d4fb819e5e593250e22c1693
mehkanarshad <mehkanarshadskp@gmail.com>
2024-09-13 11:09:01 +0500

    The new post form created

--- client/src/componenets/AppRoutes.jsx
+++ client/src/componenets/AppRoutes.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import PostList from "../features/posts/PostList";
 import { Route, Routes } from "react-router-dom";
 import PostDetail from "../features/posts/PostDetail";
+import NewPostForm from "../features/posts/NewPostForm";
 
 export default function AppRoutes() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<PostList />}></Route>
-        <Route path="/new" element={<h1>New Posts here</h1>}></Route>
+        <Route path="/new" element={<NewPostForm/>}></Route>
         <Route path="posts/:id" element={<PostDetail/>}></Route>
 
       </Routes>
---
+++ client/src/features/posts/NewPostForm.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { API_URL } from "../../Constants";
+
+export default function NewPostForm() {
+  const [title, setTilte] = useState("");
+  const [body, setBody] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const postData = {title,body};
+    const response = await fetch(API_URL,{
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(postData),
+    });
+
+    if (response.ok){
+        const {id } = await response.json();
+        navigate(`/posts/${id}`);
+    }else{
+        console.log("An error occured")
+    }
+  };
+
+  return (
+    <div>
+      <h2> Create New Post</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="titleInput">Title:</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => {
+              setTilte(e.target.value);
+            }}
+            id="titleInput"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="bodyInput">Body:</label>
+          <textarea
+            type="text"
+            id="bodyInput"
+            value={body}
+            onChange={(e) => {
+              setBody(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <button type="submit">Create Post</button>
+        </div>
+      </form>
+    </div>
+  );
+}
